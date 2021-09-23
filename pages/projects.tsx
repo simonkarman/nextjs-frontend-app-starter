7cc8365936eed757426042d9c1c4448fd7f4dfe2
Simon Karman <mail@simonkarman.nl>
2021-09-24 01:55:54 +0200

    Created Project to render both the static and dynamic projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -14,12 +14,24 @@ export const getStaticProps: GetStaticProps<{ projects: IProjectFields[] }> = as
   },
 });
 
-const Card = styled.div`
-  box-shadow: 1em;
+const Box = styled.div`
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);;
   border: 1px solid gray;
   margin: 0.5em;
   padding: 0.5em;
 `;
+
+const Project = ({ name, title, date }: IProjectFields) => (
+  <Box key={name}>
+    <LinkTo href={`/projects/${name}`}>
+      {title}
+      {' '}
+      (
+      {date}
+      )
+    </LinkTo>
+  </Box>
+);
 
 type ProjectsPageProps = InferGetStaticPropsType<typeof getStaticProps>;
 const ProjectsPage: NextPage<ProjectsPageProps> = (props: ProjectsPageProps) => {
@@ -29,36 +41,17 @@ const ProjectsPage: NextPage<ProjectsPageProps> = (props: ProjectsPageProps) => 
     <>
       <h1>Last 3 Projects (static)</h1>
       <p>Very fast because pre rendered and part of SEO, however need a rebuild to change</p>
-      {projects.map(({ name, title, date }) => (
-        <Card key={name}>
-          <LinkTo href={`projects/${name}`}>
-            {title}
-            {' '}
-            (
-            {date}
-            )
-          </LinkTo>
-        </Card>
-      ))}
+      {projects.map(Project)}
       <hr />
       <h1>Last 4 Projects (dynamic)</h1>
       <p>Slow because data needs to be fetched by client and not part of SEO, however does not need a rebuild to change</p>
       {fetching && (<div>Loading...</div>)}
       {fetching || (
         fetchedProjects?.items
-          .sort((a, b) => b.fields.date.localeCompare(a.fields.date))
+          .map((project) => project.fields)
+          .sort((a, b) => b.date.localeCompare(a.date))
           .slice(0, 4)
-          .map(({ fields: { name, title, date } }) => (
-            <Card>
-              <LinkTo href={`/projects/${name}`}>
-                {title}
-                {' '}
-                (
-                {date}
-                )
-              </LinkTo>
-            </Card>
-          ))
+          .map(Project)
       )}
     </>
   );
